96cccff93f1980777e07497f2ac0fe5d234d1d15
Andreas <Andreas@LenovoB50-80>
2016-10-07 12:51:43 +0200

    Empty Maven project

---
+++ meets/src/main/java/org/apache/maven/meetsproject/meets/App.java
@@ -0,0 +1,13 @@
+package org.apache.maven.meetsproject.meets;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
