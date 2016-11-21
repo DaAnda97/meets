f5c12212edfd8b7ff492f64177e09f2f02550821
Andreas <andreas.riepl@gmx.net>
2016-11-21 14:34:37 +0100

    created all classes, ShowUser modified

---
+++ meets/src/main/java/de/meets/vaadin_archetype_application/MeetingOverview.java
@@ -0,0 +1,16 @@
+package de.meets.vaadin_archetype_application;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.VerticalLayout;
+
+// Alle entsprechenden Meets anzeigen anzeigen
+public class MeetingOverview extends VerticalLayout implements View{
+
+	@Override
+	public void enter(ViewChangeEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- meets/src/main/java/de/meets/vaadin_archetype_application/ShowUser.java
+++ meets/src/main/java/de/meets/vaadin_archetype_application/ShowUser.java
@@ -3,52 +3,112 @@ package de.meets.vaadin_archetype_application;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 
-public class ShowUser extends VerticalLayout implements View{
+public class ShowUser extends HorizontalLayout implements View{
+	VerticalLayout informationPanel = new VerticalLayout();
+	Label message = new Label("Deine Angaben:");
+	TextField name = new TextField("Benutzername");
+	TextField email = new TextField("E-Mail");
+	TextField location = new TextField("Ort");
+	
+	HorizontalLayout buttonPanel = new HorizontalLayout();
+    Button edit = new Button("Bearbeiten");
+    Button saveChanges = new Button("Speichern");
+    Button removeChanges = new Button("Abbrechen");
+	
+    
+    VerticalLayout passwordPanel = new VerticalLayout();
+    Label changePassword = new Label("Passwort ändern:");
+    PasswordField passwordOld = new PasswordField("Altes Passwort");
+    PasswordField passwordNew = new PasswordField("Neues Passwort");
+    PasswordField passwordNewConfirm = new PasswordField("Bestätige neues Passwort");
+    Button confirmNewPassoword = new Button("Bestätigen");
+    
+    
 	@Override
 	public void enter(ViewChangeEvent event) {
-	    Label message = new Label("Deine Angaben:");
-	    
-	    TextField name = new TextField("Benutzername");
+		//-------------------- INFORMATION - PANEL --------------------------
+	    //Angaben durch Informationen zu angemedeten Benutzer ersetzen
 	    name.setValue("Dein Benutzername");
-	    name.setEnabled(false);
-	    
-	    TextField email = new TextField("E-Mail");
 	    email.setValue("Deine Mail");
-	    email.setEnabled(false);
+	    location.setValue("Dein Ort");
 	    
-	    Button edit = new Button("Bearbeiten");
-	    Button saveChanges = new Button("Speichern");
-	    Button removeChanges = new Button("Abbrechen");
+	    changeToViewMode();
 	    
+	    // Button ClickListener
 	    edit.addClickListener(e -> {
-	    	name.setEnabled(true);
-	    	email.setEnabled(true);
-	   		this.removeComponent(edit);
-	   		this.addComponents(saveChanges, removeChanges);
+	    	changeToEditMode();
 	    });
-	    
 	    saveChanges.addClickListener(e -> {
-	    	name.setEnabled(false);
-	    	email.setEnabled(false);
-	   		this.addComponent(edit);
-	   		this.removeComponent(saveChanges);
-	   		this.removeComponent(removeChanges);
+	    	saveChanges();
+	    	changeToViewMode();
 	    });
-	    
 	    removeChanges.addClickListener(e -> {
-	    	name.setEnabled(false);
-	    	email.setEnabled(false);
-	   		this.addComponent(edit);
-	   		this.removeComponent(saveChanges);
-	   		this.removeComponent(removeChanges);
+	    	changeToViewMode();
 	    });
+	    buttonPanel.addComponent(edit);
 	    
-	    this.addComponents(message, name, email, edit);
+	    informationPanel.addComponents(message, name, email, location, buttonPanel);
+	    
+	    
+	    //-------------------- PASSWORD - PANEL ---------------------------
+	    confirmNewPassoword.addClickListener(e -> {
+	    	confirmNewPassoword();
+	    });
+	    passwordPanel.addComponents(changePassword, passwordOld, passwordNew, passwordNewConfirm, confirmNewPassoword);
+	    
+	    
+	    //------------------------ MAIN - PANEL ---------------------------
+	    
+	    this.addComponents(informationPanel, passwordPanel);
 	    this.setMargin(true);
 	    this.setSpacing(true);
+	}
+
+	private void saveChanges() {
+		String nameValue = name.getValue();
+		String emailValue = email.getValue();
+		String locationValue = location.getValue();
+		
+		//TODO Methodenaufruf an SQLAgent, der Änderungen speichert
+	}
+
+	private void changeToViewMode() {
+		name.setEnabled(false);
+    	email.setEnabled(false);
+    	location.setEnabled(false);
+    	buttonPanel.addComponent(edit);
+   		buttonPanel.removeComponent(saveChanges);
+   		buttonPanel.removeComponent(removeChanges);
+	}
+	
+	private void changeToEditMode() {
+		name.setEnabled(true);
+    	email.setEnabled(true);
+    	location.setEnabled(true);
+    	buttonPanel.removeComponent(edit);
+    	buttonPanel.addComponents(saveChanges, removeChanges);
+	}
+
+	private void confirmNewPassoword() {
+		String oldPassword = name.getValue();
+		String newPassword = email.getValue();
+		String newPasswordConfirm = location.getValue();
+		
+		if (newPassword.equals(newPasswordConfirm)){
+			//TODO Stimmt altesPasswort -> speichern
+			
+		} else {
+			//TODO Fehlermeldung
+		}
+		
+		passwordOld.setValue("");
+		passwordNew.setValue("");
+		passwordNewConfirm.setValue("");
 	}
 }
