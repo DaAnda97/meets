1379108d828e26493f69693122caca9604cffae2
Sebastian Luther <luthesebas@gmail.com>
2016-11-24 12:58:46 +0100

    Check Member Fields
    Methods to check if username and email is whether in use or not

--- meets/src/main/java/de/meets/asset_manager/MemberManager.java
+++ meets/src/main/java/de/meets/asset_manager/MemberManager.java
@@ -13,12 +13,69 @@ public final class MemberManager extends AssetManager<Member> {
 	public MemberManager() {
 		super("Member");
 	}
+	
+	@Override
+	public int add( Member asset ) {
+		asset.setUsername( asset.getUsername().toLowerCase() );
+		asset.setEmail( asset.getEmail().toLowerCase() );
+		return super.add(asset);
+	}
+	
+	// check if username exists already
+	public boolean existsUsername( String username ) {
+		long count = this.count("username", username.toLowerCase());
+		
+		if ( count == 0L ) {
+			// username NOT exists
+			return false;
+		} else {
+			// username exists
+			return true;
+		}
+	}
 
+	// check if email exists already
+	public boolean existsEMail( String email ) {
+		long count = this.count("email", email.toLowerCase());
+		
+		if ( count == 0L ) {
+			// username NOT exists
+			return false;
+		} else {
+			// username exists
+			return true;
+		}
+	}
+	
+	private long count( String column, String value ) {
+		Session session = this.getFactory().openSession();
+		Transaction tx = null;
+		long count = 0;
+		
+		try {
+			tx = session.beginTransaction();
+			count = (long) session.createQuery("SELECT COUNT(*) FROM Member m WHERE m." +column +"='" +value +"'")
+					.getSingleResult();
+			tx.commit();			
+		} catch ( HibernateException e ) {
+			if ( tx != null ) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		
+		return count;
+	}
+	
 	// check if login is correct
 	public Member get( String email, String password ) {	
 		Session session = this.getFactory().openSession();
 		Transaction tx = null;
 		Member member = null;
+		
+		email = email.toLowerCase();
 		
 		try {
 			tx = session.beginTransaction();
