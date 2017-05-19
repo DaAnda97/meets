3acc9348c65589fd0bdffdc24af2f850c2e41625
Andreas <andreas.riepl@gmx.net>
2017-05-19 08:47:41 +0200

    Created class MeetingComponent

---
+++ meets/src/main/java/de/meets/gui/extendedComponents/MeetingComponent.java
@@ -0,0 +1,11 @@
+package de.meets.gui.extendedComponents;
+
+import com.vaadin.ui.CustomComponent;
+
+public class MeetingComponent extends CustomComponent{
+
+	private static final long serialVersionUID = 1L;
+
+	
+	
+}
